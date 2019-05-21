73c6d75b8d0f343c9fb5ab4a63660f9255d47e8d
albertofpena <afernandez@hggm.es>
2019-05-21 17:32:27 +0200

    Fixed error in geodesic_trace_back when path_length = 0

--- external/exactgeodesic/geodesic_trace_back.m
+++ external/exactgeodesic/geodesic_trace_back.m
@@ -11,5 +11,9 @@ d = geodesic_convert_surface_points(tmp);
 tmp = libpointer('doublePtrPtr');
 [path_length, tmp, path_double] = calllib(geodesic_library, 'trace_back', algorithm.id, d, tmp);
 
-setdatatype(path_double, 'doublePtr', path_length*5);
-path = geodesic_convert_surface_points(path_double.Value);
+if path_length > 0
+    setdatatype(path_double, 'doublePtr', path_length*5);
+    path = geodesic_convert_surface_points(path_double.Value);
+else
+    path = 0;
+end
